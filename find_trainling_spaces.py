5690c0ec523f48dd1508d5a03e4c4370dd37d34b
Nicholas Guarino <nguarino@datto.lan>
2018-06-27 14:53:41 -0400

    working with printing to file

--- find_trainling_spaces.py
+++ find_trainling_spaces.py
@@ -1,5 +1,4 @@
 import os
 
 for root, dirs, files in os.walk("/datto/array1/owncloud/dd-itcl.dattodrive.com/data/", topdown=True):
-    for name in files, dirs:
-        print(os.path.join(root, name))
+    print(root)
