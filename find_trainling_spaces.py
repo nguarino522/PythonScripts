0dc860c9b4054a9cc6809b1d53e7ec1974d3a14d
Nicholas Guarino <nguarino@datto.lan>
2018-06-27 14:35:53 -0400

    simplifying

--- find_trainling_spaces.py
+++ find_trainling_spaces.py
@@ -1,9 +1,5 @@
 import os
 
-directory_list = list()
-
 for root, dirs, files in os.walk("/datto/array1/owncloud/dd-itcl.dattodrive.com/data/", topdown=True):
-    for name in dirs:
-        directory_list.append(os.path.join(root, name))
-
-print directory_list
+    for name in files, dirs:
+        print(os.path.join(root, name))
