63ac6cc6019f7f94d9b285e8f457d01be2e46809
Nicholas Guarino <nguarino@datto.lan>
2018-06-28 09:57:18 -0400

    asdfas

--- find_trainling_spaces.py
+++ find_trainling_spaces.py
@@ -1,5 +1,15 @@
 import os
+import glob
 
-for root, dirs, files in os.walk("/datto/array1/owncloud/dd-itcl.dattodrive.com/data/", topdown=True):
-    if "*~" in root:
-        print(root)
+#for root, dirs, files in os.walk("/datto/array1/owncloud/dd-itcl.dattodrive.com/data/", topdown=True):
+#    if "* " in root:
+#        print(root)
+
+
+
+space_list = glob.glob('/datto/array1/owncloud/dd-itcl.dattodrive.com/data/* ')
+
+for i in space_list:
+    print i
+
+    
